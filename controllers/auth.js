a588ec4a9b054f569f7413b6324ad0999a85f770
Danilo P. Catalan <danilo.e.catalan@gmail.com>
2022-11-04 17:14:28 -0300

    add mongodb and moongose

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,29 @@
 const { response } = require('express');
+const User = require('../models/User');
 
-const createUser = (req, res = response) => {
-  const { name, email, password } = req.body;
+const createUser = async (req, res = response) => {
+  
+  try {
+    const user = new User(req.body);
 
-  res.status(201).json({
-    ok: true,
-    msg: 'register',
-    name,
-    email,
-    password,
-  });
+    await user.save();
+
+    res.status(201).json({
+      ok: true,
+      msg: 'register',
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Error, contact the administrator',
+    });
+  }
 };
 
 const loginUser = (req, res = response) => {
   const { email, password } = req.body;
-  
+
   res.status(200).json({
     ok: true,
     msg: 'login',
