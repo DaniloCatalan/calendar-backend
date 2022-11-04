c896ce3f685a7ea08879cbb78cd7c513f944cada
Danilo P. Catalan <danilo.e.catalan@gmail.com>
2022-11-04 15:27:27 -0300

    add validations

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,38 @@
 // host + api/auth
 
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { validateFields } = require('../middlewares/validate-fields');
 const { createUser, loginUser, renewToken } = require('../controllers/auth');
 
 const router = Router();
 
-router.post('/new', createUser);
+router.post(
+  '/new',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Email is required').isEmail(),
+    check(
+      'password',
+      'Password is required and must be more than 6 characters long'
+    ).isLength({ min: 6 }),
+    validateFields,
+  ],
+  createUser
+);
 
-router.post('/', loginUser);
+router.post(
+  '/',
+  [
+    check('email', 'Email is required').isEmail(),
+    check(
+      'password',
+      'Password is required and must be more than 6 characters long'
+    ).isLength({ min: 6 }),
+    validateFields,
+  ],
+  loginUser
+);
 
 router.get('/renew', renewToken);
 
