4cf907003d7775ab98e28ee93c1a941b0b794c8d
Danilo P. Catalan <danilo.e.catalan@gmail.com>
2022-11-04 09:10:03 -0300

    setup: exprress

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hola mundo')
+const express = require('express');
+require('dotenv').config();
+
+// Create express server (app)
+const app = express();
+
+// Public directory
+app.use(express.static('public'));
+
+// Listen requests
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running in ${process.env.PORT} port`);
+});
