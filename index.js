0a741c1f034142334ad91cd7b5556f7492cb181b
Danilo P. Catalan <danilo.e.catalan@gmail.com>
2022-11-04 09:41:50 -0300

    add endpoints

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 // Public directory
 app.use(express.static('public'));
 
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+
 // Listen requests
 app.listen(process.env.PORT, () => {
   console.log(`Server is running in ${process.env.PORT} port`);
