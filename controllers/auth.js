9518b0221c414a76f5f70af6eb22bdbbd7673281
Danilo P. Catalan <danilo.e.catalan@gmail.com>
2022-11-07 11:13:41 -0300

    feat: validate user & encrypt pass

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,32 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 const createUser = async (req, res = response) => {
-  
+  const { email, password } = req.body;
+
   try {
-    const user = new User(req.body);
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'An user exists with this email',
+      });
+    }
+
+    user = new User(req.body);
+
+    // Encrypt password
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(password, salt);
 
     await user.save();
 
     res.status(201).json({
       ok: true,
-      msg: 'register',
+      uid: user.id,
+      name: user.name,
     });
   } catch (error) {
     console.log(error);
